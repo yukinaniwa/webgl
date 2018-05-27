573304e7b1fe96350b8a4ae2bc58836212fe8103
yuki.naniwa <yuki.naniwa>
2018-05-27 21:17:36 +0900

    rim lighting

---
+++ chapter/04/js/chapter04.js
@@ -0,0 +1,110 @@
+// https://qiita.com/_nabe/items/1657d9556591284a43c8
+
+// window.addEventListener('DOMContentLoaded', init);
+window.addEventListener('load', init);
+
+function init() {
+  const width = 960;
+  const height = 540;
+
+  const renderer = initRenderer(width, height);
+  const scene = initScene(width, height);
+  const camera = initCamera(width, height, 0, 100, -400);
+  initCameraControls(renderer, camera, 0, 32, 32);
+  const stats = attachFpsView()
+
+  //
+  var progress_timer = 0;
+
+  var cubeTexture = new THREE.CubeTextureLoader()
+  	.setPath('../textures/cubemap/')
+  	.load( [
+  		'posx.jpg',
+  		'negx.jpg',
+  		'posy.jpg',
+  		'negy.jpg',
+  		'posz.jpg',
+  		'negz.jpg'
+  	] );
+  scene.background = cubeTexture;
+
+  // GUI
+  var controls = new function () {
+      this.power = 2.0;
+  };
+  var gui = new dat.GUI( { autoPlace: true } );
+  gui.add(controls, 'power', 0.5, 4.0);
+
+  // light
+  const light = new THREE.DirectionalLight(0xFFFFFF);
+  light.intensity = 1.0;
+  light.position.set(1, 3, 2);
+  scene.add(light);
+
+  // 光源位置に配置する
+  var sphereMesh = new THREE.Mesh(new THREE.SphereGeometry( 8, 64, 64), new THREE.MeshPhongMaterial({
+    color: 0xFDB813
+  }));
+  scene.add(sphereMesh);
+
+  // SHADER
+  var vLightPosition = new THREE.Vector3();
+
+  var rimPower = new THREE.Vector3();
+  let rimLighting = new THREE.ShaderMaterial({
+    vertexShader: loadShaderFile("shader/vertex.vsh"),
+    fragmentShader: loadShaderFile("shader/rim_lighting.fsh"),
+    uniforms:{
+      lightPos: {type: "v3", value: vLightPosition},
+      cameraPos: {type: "v3", value: camera.position},
+      rimPower: {type: "v3", value: rimPower},
+    },
+  });
+
+  // COLLADA
+  const loader = new THREE.ColladaLoader();
+  var colladaModel;
+  loader.load('../models/bunny.dae', (collada) => {
+    colladaModel = collada.scene;
+    colladaModel.scale.set(128,128,128);
+    colladaModel.position.set(0,0,0);
+
+    scene.add(colladaModel);
+  });
+
+  // progress
+  tick();
+
+  function tick() {
+    requestAnimationFrame(tick);
+
+    progress_timer += 0.016;
+
+    // 移動行列を作成
+    var mTrans = new THREE.Matrix4();
+    mTrans.makeTranslation(320, 220, 0);
+
+    // 回転行列を作成
+    var mRotate = new THREE.Matrix4();
+    mRotate.makeRotationAxis( new THREE.Vector3(0, 1, 0), progress_timer);
+
+    // 移動行列 * 回転行列をした行列から
+    // 移動成分を抽出する
+    var mLightPosition = mRotate.multiply(mTrans);
+    vLightPosition.setFromMatrixPosition(mLightPosition);
+
+    //
+    rimPower.set(controls.power,controls.power,controls.power);
+
+    // 光源の位置に設定
+    sphereMesh.position.set(vLightPosition.x,vLightPosition.y,vLightPosition.z);
+
+    // RIM
+    colladaModel.children.forEach(function(childModel) {
+      childModel.material = rimLighting;
+    });
+
+    renderer.render(scene, camera);
+    stats.update();
+  }
+}
---
+++ chapter/04/shader/rim_lighting.fsh
@@ -0,0 +1,27 @@
+
+// varying: fragment shader に送るもの
+// viewMatrix と cameraPosition 送らなくても fsh で使える
+
+varying mat4 vModelMatrix;       // オブジェクト座標からワールド座標へ変換する
+varying mat4 vModelViewMatrix;   // modelMatrixとviewMatrixの積算
+varying mat4 vProjectionMatrix;  // カメラの各種パラメータから３次元を２次元に射影し、クリップ座標系に変換する行列
+varying mat4 vNormalMatrix;      // 頂点法線ベクトルを視点座標系に変換する行列
+varying vec3 vPosition;          // 頂点座標
+varying vec3 vNormal;            // 頂点法線ベクトル
+varying vec2 vUv;                // テクスチャを貼るためのUV座標
+
+// uniforms 外から定義して送るもの
+uniform vec3 lightPos;
+uniform vec3 cameraPos;
+uniform vec3 rimPower;
+
+void main() {
+  vec3 eyeVector = normalize( cameraPos - vPosition );
+  vec3 lightVector = normalize(vPosition - lightPos);
+  //float half_lambert = dot(lightVector, vNormal) * 0.5 * 0.5;
+
+  float rim = (dot(vNormal, eyeVector));
+  vec3 rimColor = vec3(1.0 - rimPower.x * (rim*rim*rim*rim));
+
+  gl_FragColor = vec4(vec3(0.32, 0.68, 0.43)+rimColor, 1.0);
+}
